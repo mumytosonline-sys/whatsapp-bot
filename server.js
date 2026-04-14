1d43cb04013e67f83eac1b44f1fcada51c6a4f8d
MuCore-haras <mumytosonline@gmail.com>
2026-04-14 13:25:34 -0500

    Change server port from 3000 to 8080

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post("/webhook", (req, res) => {
 });
 
 // 🚀 SERVIDOR
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("🚀 Servidor corriendo en puerto", PORT);
 });
